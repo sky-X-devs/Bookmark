a216f0bdb03aafed58f0e5f8a3a795c5cbf66603
skytop <yashpremverma@gmail.com>
2025-01-01 00:25:27 +0530

    Add files via upload

--- src/components/feature.jsx
+++ src/components/feature.jsx
@@ -2,10 +2,12 @@ export function Feature (){
 
     return(
         <div>
-            <div>
-                <span>FEATURE</span>
+            <div className=" text-center">
+                <p className=" font-bold text-6xl py-8">Feature</p>
+                <p className="text-pretty">Our aim is to make it quick and easy for you to access your </p>
+                <p> favourite websites. Your bookmarks sync between your</p>
+                <p>devices so you can access them on the go.</p>
             </div>
-
         </div>
     )
 }
