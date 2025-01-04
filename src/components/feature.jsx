d5922370b7a0c527489c1d30c89c55eed420e4bb
skytop <yashpremverma@gmail.com>
2025-01-04 11:20:21 +0530

    feature tab improved

--- src/components/feature.jsx
+++ src/components/feature.jsx
@@ -3,6 +3,7 @@ import image1 from './../assets/illustration-features-tab-1.svg';
 import image2 from './../assets/illustration-features-tab-2.svg';
 import image3 from './../assets/illustration-features-tab-3.svg';
 export function Feature (){
+    const [ index , setIndex ] = useState(0);
     const featureTab = [{   image:image1,
         title:'Bookmark in one click',
         description:'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.'
@@ -13,6 +14,7 @@ export function Feature (){
         title:'Share your bookmarks',
         description:'Easily share your bookmarks and collections with others. Create a shareable a link that you can send at the click of a button.'
     }];
+
     return(
         <div>
             <div className=" text-center mb-16">
@@ -23,25 +25,34 @@ export function Feature (){
             </div>
             <div className="flex justify-center">
                 <div className=" absolute w-[500px] flex justify-between font-semibold text-slate-500 ">
-                    <button className="hover:text-red-400 hover:underline hover:underline-offset-8">Simple Bookmarking</button>
-                    <button className="hover:text-red-400 hover:underline hover:underline-offset-8">Speedy Searching</button>
-                    <button className="hover:text-red-400 hover:underline hover:underline-offset-8">Easy Sharing</button>
+                    <button className="hover:text-red-400 hover:underline hover:underline-offset-8"onClick={()=>{setIndex(0)}} >Simple Bookmarking</button>
+                    <button className="hover:text-red-400 hover:underline hover:underline-offset-8"onClick={()=>{setIndex(1)}} >Speedy Searching</button>
+                    <button className="hover:text-red-400 hover:underline hover:underline-offset-8"onClick={()=>{setIndex(2 )}}>Easy Sharing</button>
                 </div>
             </div>
             <div className="relative w-[535px] bg-slate-200 h-0.5 mb-10 mt-10 flex ml-[500px]"></div>
-                {featureTab.forEach(tabdata)}
-        </div>
+                <TabData  featureTab= {featureTab[index]}/>
+        </div>  
     ) 
 }
-function tabdata (index,data){
-    console.log(data)
-    console.log(index,"<- index")
+function TabData ({featureTab}){
+        
     console.log('tabdata is called ');
+    console.log(featureTab);
     return(
-        <div>
-            <img src="data[index].image" alt="" />
-            <p className="text-3xl">{data[index].title}</p>
-            <p className="text-slate-300 font-balance" >{data[index].description}</p>
+        <div className="flex" >
+            <div className=' bg-indigo-500 text-4xl w-[600px] h-64 rounded-r-full mt-48 '></div>        
+                <div className="absolute">
+                    <img className=' ml-48 mt-0' src={featureTab.image} alt={image1} />
+                </div>     
+                <div className="ml-48" >
+                    <p className="font-semibold text-4xl ">{featureTab.title}</p>
+                    <p className="w-[450px] mt-8 text-slate-400">{featureTab.description}</p>
+                    <button className="bg-indigo-500 rounded px-5 py-3 my-8 font-bold text-white border-2 border-indigo-500 hover:text-indigo-500 hover:bg-white hover:border-indigo-500 hover:border-solid">
+                        More Info   
+                    </button>
+                </div>       
+            
         </div>
     )
 }
