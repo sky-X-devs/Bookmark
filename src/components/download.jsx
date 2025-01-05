f375e978606c02366135a65de6f3afd4c5794690
skytop <yashpremverma@gmail.com>
2025-01-05 14:17:44 +0530

    download init

---
+++ src/components/download.jsx
@@ -0,0 +1,11 @@
+export function Download(){
+
+    return(
+        <div className="grid">
+            <div className="w-56 h-48 bg-blue-300"></div>
+            <div></div>
+            <div></div>
+
+        </div>
+    )
+}
