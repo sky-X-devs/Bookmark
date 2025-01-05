ba69beca8b6bce17ff1b828f034b3f8b6138aea2
skytop <yashpremverma@gmail.com>
2025-01-05 14:33:29 +0530

    Add files via upload

---
+++ download.jsx
@@ -0,0 +1,13 @@
+export function Download(){
+
+    return(
+        <div className="p-8">
+            <p className="font-semibold text-6xl py-5 px-[600px]">Download</p>
+            <div className="px-64 grid grid-cols-3 gap-10 ">
+                <div className="w-56 h-48 rounded shadow-xl"></div>   
+                <div className="w-56 h-48 rounded shadow-xl"></div>
+                <div className="w-56 h-48 rounded shadow-xl"></div>
+            </div>
+        </div>
+    )
+}
