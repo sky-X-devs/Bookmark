1bcaddbbaad92b26fb0823972574f7a131ef5700
skytop <yashpremverma@gmail.com>
2025-01-06 15:55:22 +0530

    Update download.jsx

--- src/components/download.jsx
+++ src/components/download.jsx
@@ -1,11 +1,13 @@
 export function Download(){
 
     return(
-        <div className="grid">
-            <div className="w-56 h-48 bg-blue-300"></div>
-            <div></div>
-            <div></div>
-
+        <div className="p-8">
+            <p className="font-semibold text-6xl py-5 px-[600px]">Download</p>
+            <div className="px-64 grid grid-cols-3 gap-10 ">
+                <div className="w-56 h-48 rounded shadow-xl"></div>   
+                <div className="w-56 h-48 rounded shadow-xl"></div>
+                <div className="w-56 h-48 rounded shadow-xl"></div>
+            </div>
         </div>
     )
 }
